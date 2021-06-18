9ae92656ce9a1fe6498fdeb2ad96f4c9bb241fac
前田啓 <maedakei@maedakeinoMacBook-Pro-2.local>
2021-06-19 03:05:22 +0900

    b_pizza ac

--- b_pizza.py
+++ b_pizza.py
@@ -1,13 +1,22 @@
-import sys
-
 inputs = [
-    '8',
-    '3',
-    '2',
-    '3',
-    '1',
+    # '8',
+    # '3',
+    # '2',
+    # '3',
+    # '1',
+    # '4',
+    # '6'
+
+    '20',
     '4',
-    '6'
+    '4',
+    '12',
+    '8',
+    '16',
+    '7',
+    '7',
+    '11',
+    '8'
 ]
 
 def input():
@@ -22,21 +31,27 @@ d_positions = [0]
 for i in range(n - 1):
     d_positions.append(int(input()))
 d_positions.append(d)
+d_positions.sort()
 
 deliveries_at = []
 for i in range(m):
     deliveries_at.append(int(input()))
 
-def search_delivery_distance(num):
-    most_near = sys.maxsize
-    for p in d_positions:
-        if abs(p - num) < most_near:
-            most_near = abs(p - num)
-    return most_near
+
+def binary_search_delivery_distance(num):
+    left = 0
+    right = n
+    while right - left > 1:
+        mid = int((left + right) / 2)
+        if num > d_positions[mid]:
+            left = mid
+        else:
+            right = mid
+    return min(abs(num - d_positions[right]), abs(num - d_positions[left]))
 
 result = 0
 for i in deliveries_at:
-    distance = search_delivery_distance(i)
+    distance = binary_search_delivery_distance(i)
     result += distance
 
 print(result)
