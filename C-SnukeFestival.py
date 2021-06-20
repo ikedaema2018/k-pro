65ba5c675659559af8285777cf2142da11eaa5c7
前田啓 <maedakei@maedakeinoMacBook-Pro-2.local>
2021-06-20 18:27:35 +0900

    C-SnukeFestival.py binary_search_tle

--- C-SnukeFestival.py
+++ C-SnukeFestival.py
@@ -1,8 +1,8 @@
 inputs = [
-'6',
-'3 14 159 2 6 53',
-'58 9 79 323 84 6',
-'2643 383 2 79 50 288'
+    '6',
+    '3 14 159 2 6 53',
+    '58 9 79 323 84 6',
+    '2643 383 2 79 50 288'
 ]
 
 
@@ -10,7 +10,7 @@ def input():
     return inputs.pop(0)
 
 
-N = input()
+N = int(input())
 a_s = list(map(int, input().split()))
 b_s = list(map(int, input().split()))
 c_s = list(map(int, input().split()))
@@ -18,14 +18,36 @@ a_s.sort()
 b_s.sort()
 c_s.sort()
 
+
+def binary_search_min_over_num(num, arr):
+    left = 0
+    right = len(arr) - 1
+    while right - left > 1:
+        mid = int((left + right) / 2)
+        if num >= arr[mid]:
+            left = mid
+        else:
+            right = mid
+        print(left, right)
+    if arr[left] > num:
+        while left != 0 and arr[left] == arr[left - 1]:
+            left = left - 1
+        return left
+    elif arr[right] <= num:
+        return -1
+    else:
+        return right
+
 result = 0
-for i in a_s:
-    for j in b_s:
-        if i >= j:
-            continue
-        for k in c_s:
-            if j >= k:
-                continue
+for i in range(N):
+    target_j = binary_search_min_over_num(a_s[i], b_s)
+    if target_j == -1:
+        break
+    for j in range(target_j, N):
+        target_k = binary_search_min_over_num(b_s[j], c_s)
+        if target_k == -1:
+            break
+        for k in range(target_k, N):
             result += 1
 print(result)
 
