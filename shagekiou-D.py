25b2dc126572da352baa6564341a66fcd977632a
前田啓 <maedakei@maedakeinoMacBook-Pro-2.local>
2021-06-22 00:07:57 +0900

    shagekiou-D.py なぜこれでダメなのかわからない

---
+++ shagekiou-D.py
@@ -0,0 +1,17 @@
+# https://atcoder.jp/contests/abc023/tasks/abc023_d
+inputs = [
+    '6',
+    '100 1',
+    '100 1',
+    '100 1',
+    '100 1',
+    '100 1',
+    '1 30'
+]
+
+
+def input():
+    return inputs.pop(0)
+
+
+c
