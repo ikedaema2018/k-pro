4df4958e1fc02ebd95a247ab53850ff9041e6c73
前田啓 <maedakei@maedakeinoMacBook-Pro-2.local>
2021-06-22 01:41:41 +0900

    shagekiou-D.py わからないから答えを参考に

--- shagekiou-D.py
+++ shagekiou-D.py
@@ -1,12 +1,14 @@
 # https://atcoder.jp/contests/abc023/tasks/abc023_d
+from math import ceil
+import sys
 inputs = [
-    '6',
-    '100 1',
-    '100 1',
-    '100 1',
-    '100 1',
-    '100 1',
-    '1 30'
+'6',
+'100 1',
+'100 1',
+'100 1',
+'100 1',
+'100 1',
+'1 30'
 ]
 
 
@@ -14,4 +16,26 @@ def input():
     return inputs.pop(0)
 
 
-c
+N = int(input())
+conditions = [tuple(map(int, input().split())) for _ in range(N)]
+
+
+def ok(limit):
+    limit_times = [(limit - h) / s for h, s in conditions]
+    limit_times.sort()
+    for i in range(N):
+        if limit_times[i] < i:
+            return False
+    return True
+
+low = 0
+high = sys.maxsize
+while low < high:
+    mid = (low + high) // 2
+    if ok(mid):
+        high = mid
+    else:
+        low = mid + 1
+
+
+print(low)
