beafb09f077668c477727b56f8fb1717268926af
前田啓 <maedakei@maedakeinoMacBook-Pro-2.local>
2021-07-03 13:25:48 +0900

    abc208/c.py ac 解説見た

--- abc208/c.py
+++ abc208/c.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 inputs = [
     '20',
     '7 8 1 1 4 9 9 6 8 2 4 1 1 9 5 5 5 3 6 4'
@@ -10,10 +12,21 @@ def input():
 
 N = int(input())
 conditions = list(map(int, input().split()))
-result = 0
-for i in range(N - 1):
-    for j in range(i + 1, N):
-        if conditions[i] != conditions[j]:
-            result += 1
+conditions.sort()
+result = int(N * (N - 1) / 2)
+duplicates = [0]
 
-print(result)
+before = -1
+duplicate = 1
+for i in conditions:
+    if i == before:
+        duplicate += 1
+    else:
+        if duplicate > 1:
+            duplicates.append(duplicate)
+            duplicate = 1
+    if i == conditions[-1] and duplicate > 1:
+        conditions.append(duplicate)
+    before = i
+
+print(int(result - reduce(lambda x, y: x + (y * (y - 1) / 2), duplicates)))
