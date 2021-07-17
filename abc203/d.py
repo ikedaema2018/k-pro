2472feddcaf55529a18c88fd52ef17473f0219f2
ikedaema <maedakei@maedakeinoMacBook-Pro-2.local>
2021-07-17 15:11:03 +0900

    abc203/d.py 解説見ながら

--- abc203/d.py
+++ abc203/d.py
@@ -1,26 +1,47 @@
 inputs = [
-    '3 3',
-    '1 2 3',
-    '4 5 6',
-    '7 8 9'
+    '3 2',
+    '1 7 0',
+    '5 8 11',
+    '10 4 2'
 ]
 
 def input():
     return inputs.pop(0)
 
 N, K = map(int, input().split())
-map_depths = []
+maps = []
 for _ in range(N):
-    map_depths.append(list(map(int, input().split())))
+    maps.append(list(map(int, input().split())))
 
-result = []
-for i in range(N - K + 1):
-    for j in range(N - K + 1):
-        rangee = []
-        for k in range(K):
-            for l in range(K):
-                rangee.append(map_depths[i + k][j + l])
-        rangee = list(reversed(sorted(rangee)))
-        result.append(rangee[(K**2)//2])
 
-print(sorted(result)[0])
+
+ac = 1000000000
+wa = -1
+
+L = ((K ** 2) // 2) + 1
+while wa + 1 < ac:
+    wj = (wa + ac) // 2
+    depth = [[0] * (N + 1) for _ in range(N + 1)]
+    for i in range(N):
+        for j in range(N):
+            if maps[i][j] > wj:
+                depth[i + 1][j + 1] = 1
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            depth[i][j] = depth[i][j - 1] + depth[i][j]
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            depth[j][i] = depth[j - 1][i] + depth[j][i]
+
+    flag = False
+    for i in range(K, N + 1):
+        for j in range(K, N + 1):
+            if L > (depth[i][j] - depth[i - K][j] - depth[i][j - K] + depth[i - K][j - K]):
+                flag = True
+
+    if flag:
+        ac = wj
+    else:
+        wa = wj
+
+print(ac)
