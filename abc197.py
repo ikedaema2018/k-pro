387a826af890d697a1a0f9a586d2b39bd4fa3fbd
ikedaema <ikedaema0817@gmail.com>
2021-08-03 01:47:22 +0900

    bit全探索に tle

--- abc197.py
+++ abc197.py
@@ -12,23 +12,41 @@ N = int(input())
 A = list(map(int, input().split()))
 import sys
 m = sys.maxsize
-for i in range(2 ** N):
-    S = []
-    G = []
-    for j in range(N):
+for i in range(2 ** (N - 1)):
+    i_s = []
+    for j in range(N - 1):
         if (i >> j) & 1:
-            S.append(A[j])
+            i_s.append(j + 1)
+    if len(i_s) == 0:
+        continue
+    S = []
+    pre = -1
+    for j in range(len(i_s)):
+        d = i_s[j]
+        if pre == -1 and j == (len(i_s) - 1):
+            S.append(A[0:d])
+            S.append(A[d:])
+        elif pre == -1:
+            S.append(A[0:d])
+        elif j == (N - 1):
+            S.append(A[pre:d])
+            S.append(A[d:])
         else:
-            G.append(A[j])
-    if len(S) <= 0:
+            S.append(A[pre:d])
+        pre = d
+    ss = []
+    for s in S:
+        if len(s) == 0:
+            continue
+        sss = s[0]
+        for ssss in s[1:]:
+            sss = sss | ssss
+        ss.append(sss)
+    if len(ss) == 0:
         continue
-    if len(G) <= 0:
-        continue
-    s_or = S[0]
-    for s_i in range(1, len(S)):
-        s_or = s_or | S[s_i]
-    g_or = G[0]
-    for g_i in range(1, len(G)):
-        g_or = g_or | G[g_i]
-    m = min(m, (s_or ^ g_or))
+    xx = ss[0]
+    for x in ss[1:]:
+        xx = xx ^ x
+    m = min(m, xx)
 print(m)
+
