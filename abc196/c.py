0836e8c4ed053332aae67ced17101802e97f46a2
ikedaema <ikedaema0817@gmail.com>
2021-08-06 02:10:41 +0900

    abc196/c.py 解説見た

---
+++ abc196/c.py
@@ -0,0 +1,15 @@
+inputs = [
+    '111',
+]
+
+
+def input():
+    return inputs.pop(0)
+
+
+N = int(input())
+
+for i in range(1, 1000001):
+    if int(str(i) * 2) > N:
+        print(i - 1)
+        exit()
