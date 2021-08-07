8c66688e1aed5f24a0e133af0c6c37c51a0c9fac
ikedaema <ikedaema0817@gmail.com>
2021-08-07 23:46:19 +0900

    abc193/c.py 解説見た

--- abc193/c.py
+++ abc193/c.py
@@ -11,15 +11,11 @@ import math
 N = int(input())
 sqrt = math.floor(math.sqrt(N))
 c = []
-for i in range(2, sqrt + 1):
-    w = 2
-    r = i ** w
-    while r <= N:
-        if r not in c:
-            c.append(r)
-        w += 1
-        r = i ** w
 
-print(N - len(c))
+for i in range(2, sqrt + 1):
+    for j in range(2, math.floor(math.log(N, i)) + 1):
+        c.append(i ** j)
+
+print(N - len(set(c)))
 
 
