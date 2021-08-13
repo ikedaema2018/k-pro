a5d55b996c11b012ab63f8f691b22394fb965603
ikedaema <ikedaema0817@gmail.com>
2021-08-14 01:40:05 +0900

    abc190/c.py ac ヒントみた

---
+++ abc190/c.py
@@ -0,0 +1,73 @@
+# inputs = [
+#     '4 4',
+#     '1 2',
+#     '1 3',
+#     '2 4',
+#     '3 4',
+#     '3',
+#     '1 2',
+#     '1 3',
+#     '2 3',
+# ]
+
+# inputs = [
+#     '4 4',
+#     '1 2',
+#     '1 3',
+#     '2 4',
+#     '3 4',
+#     '4',
+#     '3 4',
+#     '1 2',
+#     '2 4',
+#     '2 4',
+# ]
+
+inputs = [
+    '6 12',
+    '2 3',
+    '4 6',
+    '1 2',
+    '4 5',
+    '2 6',
+    '1 5',
+    '4 5',
+    '1 3',
+    '1 2',
+    '2 6',
+    '2 3',
+    '2 5',
+    '5',
+    '3 5',
+    '1 4',
+    '2 6',
+    '4 6',
+    '5 6'
+]
+
+
+def input():
+    return inputs.pop(0)
+
+# Nは皿の個数, Mは条件
+N, M = map(int, input().split(' '))
+ms = [list(map(int, input().split(' '))) for _ in range(M)]
+K = int(input())
+candidates = [list(map(int, input().split(' '))) for _ in range(K)]
+
+ans = 0
+for bit in range(2 ** K):
+    selected = []
+    for j in range(K):
+        if (bit >> j) & 1:
+            selected.append(candidates[j][0])
+        else:
+            selected.append(candidates[j][1])
+    count = 0
+    for m in range(M):
+        if ms[m][0] in selected and ms[m][1] in selected:
+            count += 1
+    ans = max(count, ans)
+print(ans)
+
+
