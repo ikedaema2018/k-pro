0337775be76c4e31760722d6f19f90801068564b
ikedaema <ikedaema0817@gmail.com>
2021-08-14 17:12:46 +0900

    abc189/c.py tle

---
+++ abc189/c.py
@@ -0,0 +1,20 @@
+inputs = [
+    '6',
+    '200 4 4 9 4 9'
+]
+
+
+def input():
+    return inputs.pop(0)
+
+
+N = int(input())
+A = list(map(int, input().split()))
+
+ans = 0
+for i in range(N):
+    for j in range(i, N):
+        m = min([A[mi] for mi in range(i, j + 1)])
+        ans = max(ans, m * (j - i + 1))
+
+print(ans)
