200da63130ccb0899ba9b141029baaa911e8b439
ikedaema2018 <ikedaema0817@gmail.com>
2021-08-21 23:31:46 +0900

    abc178/c.py 解説見た

--- abc178/c.py
+++ abc178/c.py
@@ -7,12 +7,15 @@ def input():
     return inputs.pop(0)
 
 
-import math
-# 2つの順列の数 * 9 ** 残りの桁数
 N = int(input())
-if N < 2:
-    print(0)
-else:
-    # 2つの順列の数
-    a = math.factorial(N) // math.factorial(N - 2)
-    print(a * 9 ** (N - 2))
+mod = 10**9 + 7
+a = pow(10, N, mod)
+b = pow(9, N, mod)
+c = pow(9, N, mod)
+d = pow(8, N, mod)
+print((a - b - c + d) % mod)
+
+# ans = pow(10, N, mod)
+# ans -= pow(9, N, mod) * 2
+# ans += pow(8, N, mod)
+# print(ans % mod)
