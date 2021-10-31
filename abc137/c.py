80c0f62371a9755b68b2fcd4f8775ce21bb4465f
ikedaema2018 <ikedaema0817@gmail.com>
2021-10-31 15:03:22 +0900

    abc/137 ac 解説見た

--- abc137/c.py
+++ abc137/c.py
@@ -10,43 +10,25 @@ inputs = [
 
 def input():
     return inputs.pop(0)
-
-
 N = int(input())
 S = []
 for _ in range(N):
-    S.append(input())
+    S.append("".join(sorted(input())))
 
-counted_s = []
+S = sorted(S)
+result = 0
+
+before_s = ''
+count = 0
 for s in S:
-    _s = list(s)
-    h = {}
-    for c in _s:
-        if not h.get(c):
-            h[c] = 1
-        else:
-            h[c] += 1
-    counted_s.append(h)
-
-result = 0
-from itertools import combinations
-
-def match(_a, _b):
-    a_keys = _a.keys()
-    b_keys = _b.keys()
-    if len(a_keys) != len(b_keys):
-        return False
-    for a_key in a_keys:
-        if not (a_key in _b) or _a[a_key] != _b[a_key]:
-            return False
+    if s == before_s:
+        count += 1
     else:
-        return True
-
-result = 0
-for counted_2 in combinations(counted_s, 2):
-    a = counted_2[0]
-    b = counted_2[1]
-    if match(a, b):
-        result += 1
-
+        if count > 1:
+            result += count * (count - 1) // 2
+        count = 1
+        before_s = s
+else:
+    if count > 1:
+        result += count * (count - 1) // 2
 print(result)
