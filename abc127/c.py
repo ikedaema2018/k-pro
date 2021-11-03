b0750a2abad5300072219c86b3f0e0e8a5051ae9
ikedaema2018 <ikedaema0817@gmail.com>
2021-11-03 16:50:53 +0900

    abc127 想定回答のいもす法でもacしておく

--- abc127/c.py
+++ abc127/c.py
@@ -7,17 +7,16 @@ inputs = [
 def input():
     return inputs.pop(0)
 
+import itertools
 N, M = map(int, input().split(' '))
 lrs = [list(map(int, input().split(' '))) for _ in range(M)]
-ll = [''] * N
-rr = [''] * N
+acc = [0] * (N + 1)
 
-for i in reversed(range(len(lrs))):
-    lr = lrs[i]
+for lr in lrs:
     l, r = lr
-    ll[l - 1] = 'l'
-    rr[r - 1] = 'r'
+    acc[l - 1] += 1
+    acc[r] -= 1
 
-l_idx = N - 1 - list(reversed(ll)).index('l')
-r_idx = rr.index('r')
-print(max(0, r_idx - l_idx + 1))
+acc_result = list(itertools.accumulate(acc))
+
+print(acc_result.count(M))
