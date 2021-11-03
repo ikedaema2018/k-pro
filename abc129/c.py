8450194e797533fd65b444fc594597249ac51477
ikedaema2018 <ikedaema0817@gmail.com>
2021-11-03 12:02:57 +0900

    abc129/c.py ac 解説見た 動的探索ほう fix

--- abc129/c.py
+++ abc129/c.py
@@ -14,14 +14,16 @@ def input():
 
 div = 1000000007
 N, M = map(int, input().split(' '))
-A = [int(input()) for _ in range(M)]
+A = [True for _ in range(N + 1)]
+for _ in range(M):
+    A[int(input())] = False
 
 dp = [0] * (N + 1)
 dp[0] = 1
-dp[1] = 1 if 1 not in A else 0
+dp[1] = 1 if A[1] else 0
 
 for n in range(2, N + 1):
-    dp[n] = (dp[n - 1] if n - 1 not in A else 0) + (dp[n - 2] if n - 2 not in A else 0)
+    dp[n] = (dp[n - 1] if A[n - 1] else 0) + (dp[n - 2] if A[n - 2] else 0)
     dp[n] = dp[n] % div
 
 print(dp[N])
