afd23db8ec61e13aee8fec348e84bc1455965f60
ikedaema2018 <ikedaema0817@gmail.com>
2021-11-14 23:43:11 +0900

    re_challenge_1/abc202/c.py 解説見てリファクタ

--- re_challenge_1/abc202/c.py
+++ re_challenge_1/abc202/c.py
@@ -10,27 +10,14 @@ def input():
     return inputs.pop(0)
 
 N = int(input())
-A = list(map(int, input().split(' ')))
-# A[i]と比較するB[C[j]]の数字をkeyに、添字からCの値を逆算するために値にindexの配列を格納
-
-_B = list(map(int, input().split(' ')))
-# Bのindexと同じ値を算出するために、C[値] = 該当の値が出てくる回数にする
-C = {}
-for i in list(map(int, input().split(' '))):
-    C[i] = C[i] + 1 if i in C else 1
-
-# Bと一致するCの数を計算しておく
-B = {}
-for i in range(1, len(_B) + 1):
-    in_C_count = C[i] if i in C else 0
-    if _B[i - 1] in B:
-        B[_B[i - 1]] += in_C_count
-    else:
-        B[_B[i - 1]] = in_C_count
-
+import collections
+A = collections.Counter(list(map(int, input().split(' '))))
+B = list(map(int, input().split(' ')))
+C = list(map(int, input().split(' ')))
 result = 0
-for a in A:
-    result += (B[a] if a in B else 0)
-
+for i in range(len(C)):
+    c = C[i]
+    b = B[c - 1]
+    result += A[b]
 
 print(result)
