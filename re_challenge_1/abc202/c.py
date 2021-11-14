6c65fbc3f9f9baf1718ae82792d81d467a26f573
ikedaema2018 <ikedaema0817@gmail.com>
2021-11-14 17:27:18 +0900

    re1 abc202/c.py ac

--- re_challenge_1/abc202/c.py
+++ re_challenge_1/abc202/c.py
@@ -1,8 +1,8 @@
 inputs = [
     '3',
-    '1 2 2',
-    '3 1 2',
-    '2 3 2'
+    '2 3 3',
+    '1 3 3',
+    '1 1 1'
 ]
 
 
@@ -12,24 +12,25 @@ def input():
 N = int(input())
 A = list(map(int, input().split(' ')))
 # A[i]と比較するB[C[j]]の数字をkeyに、添字からCの値を逆算するために値にindexの配列を格納
-B = {}
-_B = list(map(int, input().split(' ')))
-for i in range(1, len(_B) + 1):
-    B[_B[i - 1]] = B[_B[i - 1]] + [i] if _B[i - 1] in B else [i]
 
+_B = list(map(int, input().split(' ')))
 # Bのindexと同じ値を算出するために、C[値] = 該当の値が出てくる回数にする
 C = {}
 for i in list(map(int, input().split(' '))):
     C[i] = C[i] + 1 if i in C else 1
 
+# Bと一致するCの数を計算しておく
+B = {}
+for i in range(1, len(_B) + 1):
+    in_C_count = C[i] if i in C else 0
+    if _B[i - 1] in B:
+        B[_B[i - 1]] += in_C_count
+    else:
+        B[_B[i - 1]] = in_C_count
+
 result = 0
 for a in A:
-    b_idxs = B[a] if a in B else []
-    _result = 0
-    for i in range(len(b_idxs)):
-        _result += C[b_idxs[i]] if b_idxs[i] in C else 0
-
-    result += _result
+    result += (B[a] if a in B else 0)
 
 
 print(result)
