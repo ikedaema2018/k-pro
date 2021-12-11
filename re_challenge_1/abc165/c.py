b6a227ceee7e29aaa9fd62476da55ec1036c00d5
ikedaema2018 <ikedaema0817@gmail.com>
2021-12-12 00:05:41 +0900

    re_challenge_1/abc165/c.py 解説見た ac

---
+++ re_challenge_1/abc165/c.py
@@ -0,0 +1,38 @@
+inputs = [
+    '3 4 3',
+    '1 3 3 100',
+    '1 2 2 10',
+    '2 3 2 10'
+]
+
+
+def input():
+    return inputs.pop(0)
+
+
+N, M, Q = map(int, input().split(' '))
+condition = []
+for _ in range(Q):
+    condition.append(list(map(int, input().split(' '))))
+
+import itertools
+A = list(itertools.combinations_with_replacement(range(1, M + 1), N))
+# print(len(A))
+# print(A)
+
+# A = []
+# for i in range(1, M + 1):
+#     A.append([i] * N)
+# A = sum(A, [])
+# _A = list(itertools.combinations(A, N))
+# print(len(_A))
+# print(_A)
+
+ans = 0
+for _a in A:
+    result = 0
+    for a, b, c, d in condition:
+        if _a[b - 1] - _a[a - 1] == c:
+            result += d
+        ans = max(ans, result)
+print(ans)
